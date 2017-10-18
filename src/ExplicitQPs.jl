b59c0259fb70a48c7e48217c3a221baf147bfd9b
Robin Deits <robin.deits@gmail.com>
2017-10-18 16:47:49 -0400

    todo

--- src/ExplicitQPs.jl
+++ src/ExplicitQPs.jl
@@ -126,6 +126,8 @@ function explicit_solution(m::Model, params::AbstractArray{Variable}, eps=1e-3)
     
     Q = sparse(getcol.(m.obj.qvars1), getcol.(m.obj.qvars2), m.obj.qcoeffs, nvars, nvars)
     Q = 0.5 .* (Q .+ Q')
+
+    # todo: cholesky instead?
     H = lufact(Q[.!isparam, .!isparam])
 
     F = Q[.!isparam, isparam]
